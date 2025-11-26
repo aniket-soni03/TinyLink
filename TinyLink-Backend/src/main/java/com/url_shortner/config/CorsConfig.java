b770448c985ac297367d6ca60df7d0f29088ed97
aniket-soni <aniket080soni@gmail.com>
2025-11-27 01:41:11 +0530

    TinyLink Version 1 26/NOV/25 01:31 AM

--- TinyLink-Backend/src/main/java/com/url_shortner/config/CorsConfig.java
+++ TinyLink-Backend/src/main/java/com/url_shortner/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173",
+                		"https://tiny-link-aniket.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
